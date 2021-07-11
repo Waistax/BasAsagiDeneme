7bb9fec6a16e6463cedb4b856c7a6359cae219cc
Cem GEÇGEL <aa-albara@hotmail.com>
2021-07-11 08:21:20 +0300

    1.1.8
    Renkleri daha iyi görmek için gölgelendirici değiştirildi.

--- kod/başaşağıderebeyi/deneme/Deneme.java
+++ kod/başaşağıderebeyi/deneme/Deneme.java
@@ -22,7 +22,7 @@ public class Deneme {
 	/** Ara sürümü. */
 	public static final int ARA_SÜRÜMÜ = 1;
 	/** Yaması. */
-	public static final int YAMASI = 7;
+	public static final int YAMASI = 8;
 	/** Bütün sürümü. */
 	public static final String SÜRÜM =
 		ANA_SÜRÜMÜ + "." + ARA_SÜRÜMÜ + "." + YAMASI;
