52d36eb8c95812485b984f21c127451d9506d73b
Cem GEÇGEL <aa-albara@hotmail.com>
2021-07-15 20:09:57 +0300

    2.0.0
    Denemenin bitmiş sürümü.

--- kod/başaşağıderebeyi/deneme/Deneme.java
+++ kod/başaşağıderebeyi/deneme/Deneme.java
@@ -19,11 +19,11 @@ import java.util.*;
 @Uygulama(adı = "deneme")
 public class Deneme {
 	/** Ana sürümü. */
-	public static final int ANA_SÜRÜMÜ = 1;
+	public static final int ANA_SÜRÜMÜ = 2;
 	/** Ara sürümü. */
-	public static final int ARA_SÜRÜMÜ = 6;
+	public static final int ARA_SÜRÜMÜ = 0;
 	/** Yaması. */
-	public static final int YAMASI = 2;
+	public static final int YAMASI = 0;
 	/** Bütün sürümü. */
 	public static final String SÜRÜM =
 		ANA_SÜRÜMÜ + "." + ARA_SÜRÜMÜ + "." + YAMASI;
